f7f290c4ae41a6109de0678f85c4d8bc534f787b
Rcalaf <rcalaf.cot@gmail.com>
2013-03-01 23:37:54 +0100

    mailer

--- config/initializers/setup_mailer.rb
+++ config/initializers/setup_mailer.rb
@@ -1,13 +1,19 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   :address => 'smtp.canu.se',
-  :port => 587,
+  :port => 25,
   :domain => 'canu.se',
-  :authentication => :plain,
+  :authentication => :login,
   :user_name => 'getgoing@canu.se',
   :password => 'Itshalfpast8',
-  #:enable_starttls_auto => true
+  :enable_starttls_auto => true
 }
 
 ActionMailer::Base.default_url_options[:host] = "canu.se"
 
+
+=begin
+Net::SMTP.start('smtp.canu.se', 25, 'canu.se','getgoing@canu.se', 'Itshalfpast8', :plain) do |smtp|
+  smtp.send_message "mola", 'getgoing@canu.se', 'rcalaf.cot@gmail.com'
+end
+=end
