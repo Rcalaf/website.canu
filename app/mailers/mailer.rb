0c8a0b68d858622e8c8bc14d967d454c140765b9
Rcalaf <rcalaf.cot@gmail.com>
2013-09-25 18:41:13 +0200

    added mailer

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,10 +1,14 @@
 # encoding: UTF-8
 class Mailer < ActionMailer::Base
-  default from: 'Get going <getgoing@canu.se>'
+  default from: 'CANU <gettogether@canu.se>'
 
   def new_user_mail
     @user = user
     mail(:to => "poluzhnikov.vitali@gmail.com", :subject => user.email)
+  end
+  
+  def info_mail(email)
+    mail(:to => email, :subject => 'Time to get together!')
   end
  
 end
