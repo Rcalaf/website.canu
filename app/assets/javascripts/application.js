4471a4626f9bd397c4398d8f551ee851ed477dfa
Vitali <vitali@canu.se>
2013-12-11 17:28:56 +0100

    mobile styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,8 @@
 
 	
 	$(document).ready(function(){
+		
+		
 			
 		$("img.phone").fadeIn(1800);
 
@@ -37,13 +39,14 @@
 		
 	   
 	    $("#feedback_btn").click(function(){
+	        $.fn.fullpage.moveToSlide('CANU');	    	
 	    	$("#comments_container").show();
 	        $("#comments_container").animate({right:"0px"},500); 
 	        $("#comments_blur").fadeIn(300); 
 	        $("#comments_description").fadeIn(900);
 	        $("#close_btn").fadeIn(1000); 
-	        $("html, body, #comments_container, #comments").animate({ scrollTop: 0 }, "slow");
-	        return false     		
+	        $("html, body").animate({ scrollTop: 0 }, "fast");
+
 	    });
 	    
 	    $("#close_btn, #comments_blur, #comments_description").click(function(){
@@ -56,6 +59,8 @@
 	    });
 	    
 	   	 $('#comments').height() - 170;
+
+	   		   	 
 	   	 
 	   	 $(".android").click(function(){
 	   	 	$(this).toggleClass("blank");
@@ -66,7 +71,9 @@
 
 	   	 
 	});
-  
+	
+
+
 
 	
      
