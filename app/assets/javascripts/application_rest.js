3858741ea034252b0aacb46f96e036218d0fb737
Vitali <vitali@canu.se>
2013-12-19 17:58:56 +0100

    added members

--- app/assets/javascripts/application_rest.js
+++ app/assets/javascripts/application_rest.js
@@ -45,6 +45,9 @@
 		$("#vit").hover(function(){ $('#vit_story').fadeToggle(10); });  
 		$("#seb").hover(function(){ $('#seb_story').fadeToggle(10); });  
 		
+		//$("#viv").hover(function(){ $('#viv_story').fadeToggle(10); });  
+		//$("#lea").hover(function(){ $('#lea_story').fadeToggle(10); });  
+		
 		$("#submit_reset").click(function() {
 		    $(".authorization_form, #reset_intro").fadeOut(100);
 		    $("#reset_confirmation").fadeIn(700);
