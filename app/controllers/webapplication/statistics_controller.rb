04dfd178ee7947113c6b48b448baa2b283c2336d
Rcalaf <rcalaf.cot@gmail.com>
2014-02-03 13:34:37 +0100

    fix data string

--- app/controllers/webapplication/statistics_controller.rb
+++ app/controllers/webapplication/statistics_controller.rb
@@ -4,8 +4,8 @@ class Webapplication::StatisticsController < Webapplication::WebapplicationContr
   def index
     @body_class = "stats"
     respond_local = request_to_canu_api("http://api.canu.se/activities",:get,{},{})
-    respond_data = JSON.parse(respond_local.body)
+    @data = JSON.parse(respond_local.body)
     #data = '{"user":{"total":17,"total_phone_verified":1,"total_email_verified":1, "ghost":5, "not_verified":16},"activity":{"total":79,"total_active":1,"active_activities_locations":[{"activity":{"latitude":-42.605337,"longitude":-21.931285}}]}}'
-    @data = JSON.parse(respond_data) 
+ 
   end
 end
