1d1e9da80bff48c3ba7e7b619bc41f579d51f0b4
Rcalaf <rcalaf.cot@gmail.com>
2014-03-04 21:40:05 +1100

    remove login

--- app/controllers/webapplication/activities_controller.rb
+++ app/controllers/webapplication/activities_controller.rb
@@ -1,5 +1,5 @@
 class Webapplication::ActivitiesController < Webapplication::WebapplicationController
-  before_filter :authenticate_user
+  #before_filter :authenticate_user
   
   def index
     puts session[:user]
